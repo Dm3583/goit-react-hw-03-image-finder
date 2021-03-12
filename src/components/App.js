b5285a674df12b46911ba771df90132aed98f027
Dm3583 <pankovdm@gmail.com>
2021-03-12 20:10:13 +0200

    start project

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Searchbar from './Searchbar';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Hello everybody!!!</h1>
+      <Searchbar />
     </div>
   );
 }
